3542468afb91091016a8cf58ba8c4c8439267719
jferracini <jferracini@yahoo.com.br>
2018-05-30 21:13:26 -0300

    Lint TS

--- src/app/app-widget-donation/app-widget-donation.component.ts
+++ src/app/app-widget-donation/app-widget-donation.component.ts
@@ -1,4 +1,4 @@
-import {BrowserModule, DomSanitizer} from '@angular/platform-browser'
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
@@ -14,9 +14,10 @@ export class AppWidgetDonationComponent implements OnInit {
   name = 'Widget Donate';
 
   animals = [
-       {id: 1, value: "giraffe" , name: "A giraffe"},
-       {id: 2, value: "rhino", name: "A rhino"},
-       {id:3, value: "tiger", name: "A tiger"},
+       {id: 1, value: 'giraffe' , name: 'A giraffe'},
+       {id: 2, value: 'rhino', name: 'A rhino'},
+       {id: 3, value: 'tiger', name: 'A tiger'},
+       {id: 4, value: 'leopard', name: 'A leopard'},
      ];
 
   form: FormGroup;
@@ -24,7 +25,7 @@ export class AppWidgetDonationComponent implements OnInit {
   constructor() {
     this.form = new FormGroup({
       animal: new FormControl(null)
-    })
+    });
   }
 
 
@@ -35,21 +36,23 @@ export class AppWidgetDonationComponent implements OnInit {
   ngOnInit() {
   }
 
-  public changeImg(value){
+  public changeImg(value) {
     console.log(value);
-    if (value == '1') {
+    if (value === '1') {
       this.imgSrc = {
         'background-image': !this.animals ? 'url(\'assets/giraffe-image.jpg\')' : 'url(\'assets/giraffe-image.jpg\')',
       };
-    }
-    else if (value == '2') {
+    } else if (value === '2') {
       this.imgSrc = {
         'background-image': !this.animals ? 'url(\'assets/rhino-image.jpg\')' : 'url(\'assets/rhino-image.jpg\')',
       };
-    }
-    else if (value == '3') {
+    } else if (value === '3') {
       this.imgSrc = {
         'background-image': !this.animals ? 'url(\'assets/tiger-image.jpg\')' : 'url(\'assets/tiger-image.jpg\')',
+      };
+    } else if (value === '4') {
+      this.imgSrc = {
+        'background-image': !this.animals ? 'url(\'assets/leopard-image.jpg\')' : 'url(\'assets/leopard-image.jpg\')',
       };
     }
   }
